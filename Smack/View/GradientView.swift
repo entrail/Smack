23e2368d48ee0ae5a31de1a41dc937b71587a973
Adrian horstmann <Adrian@adrians-mbp>
2017-08-28 21:35:29 +0200

    Added a custom gradientView to reuse it for different views
    Added ui elements for the sidebar

---
+++ Smack/View/GradientView.swift
@@ -0,0 +1,41 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by Adrian horstmann on 28.08.17.
+//  Copyright © 2017 Adrian Horstmann. All rights reserved.
+//
+
+import UIKit
+
+//IBDesignable is used to show changes done in code to the view within the interface designer
+@IBDesignable
+class GradientView: UIView {
+
+     //using Color Literal
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.2901960784, green: 0.3019607843, blue: 0.8470588235, alpha: 1){
+        didSet {
+            //updates the layout everytime color is changed
+            self.setNeedsLayout()
+        }
+    }
+    
+    //using Color Literal
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.1725490196, green: 0.831372549, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+        
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+
+}
