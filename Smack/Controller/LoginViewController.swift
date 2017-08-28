7fee6284ac2315579d591b3942ae17f8e0d2c413
Adrian horstmann <Adrian@p200300c03bf567300495d5a76f2001ab.dip0.t-ipconnect.de>
2017-08-28 22:05:58 +0200

    Added loginViewController Finished loginViewController design added functionality to dismiss login screen

---
+++ Smack/Controller/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Smack
+//
+//  Created by Adrian horstmann on 28.08.17.
+//  Copyright © 2017 Adrian Horstmann. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Adrian horstmann on 28.08.17.
+//  Copyright © 2017 Adrian Horstmann. All rights reserved.
+//
+
+import Foundation
+
+//SEGUES
+let TO_LOGIN = "toLogin"
