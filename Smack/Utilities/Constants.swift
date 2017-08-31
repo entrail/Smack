7d45ba594e556ca384bd65fb36b3aed5f72b25bd
Adrian horstmann <Adrian@adrians-mbp>
2017-08-31 20:11:50 +0200

    Added authentication service

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,7 +8,19 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL CONSTANTS
+let BASE_URL = "https://smack-de-horstmann.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+
 //SEGUES
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND_CREATE_ACCOUNT = "unwindToChannel"
+
+//USER DEFAULTSFoundation
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
